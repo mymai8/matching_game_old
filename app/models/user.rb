74d6baafd3a9eec4bee2b5e194092b56e58d5cb0
mymai8 <mywork1868@gmail.com>
2023-12-11 14:45:52 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :game_name, presence: true
+
+  has_many :coaches
+  has_many :orders
 end
