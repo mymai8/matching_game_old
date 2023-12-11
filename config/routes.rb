0335dd3f05d5f79f952eb345b4af9953c7a31ea1
mymai8 <mywork1868@gmail.com>
2023-12-11 14:43:17 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'coaches#index'
+  resources :users, only:[:edit, :update]
+  resources :coaches do
+    resources :orders, only: [:index, :create]
+    #resources :comments, only: [:create, :delete]
+  end
 end
